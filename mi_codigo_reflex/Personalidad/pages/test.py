eaf92701c43a324bafeb99751a6fa196e35b6b52
Carlos Esquivias <carlosesquivias@prefor.local>
2026-04-21 12:51:55 +0200

    resultados antiguos perso

--- mi_codigo_reflex/Personalidad/pages/test.py
+++ mi_codigo_reflex/Personalidad/pages/test.py
@@ -51,9 +51,9 @@ def index():
                         rx.heading(
                             "Progreso Test Personalidad",
                         ),
-                        margin_bottom=Size.DEFAULT
+                        margin_bottom=Size.DEFAULT.value
                     ),
-                    show_progress_test(TestState.current_progress, Size.DEFAULT),
+                    show_progress_test(TestState.current_progress, Size.DEFAULT.value),
                     rx.box(
                         rx.vstack(
                             rx.vstack(
@@ -72,7 +72,7 @@ def index():
                                         alert_dialog("Finalizar test", "¿Has revisado todas tus respuestas? Esta acción no es reversible.", "Revisar", "Sí, finalizar", TestState.finalizar_test),
                                         round_button_right_icon("Siguiente", "arrow-big-right", TestState.next_page),
                                     ),
-                                    margin_top=Size.MEDIUM_BIG,
+                                    margin_top=Size.MEDIUM_BIG.value,
                                     width="100%"
                                 )
                             ),
@@ -85,14 +85,14 @@ def index():
                                         rx.fragment(),
                                         round_button_right_icon("Siguiente", "arrow-big-right", TestState.next_page),
                                     ),
-                                    margin_top=Size.MEDIUM_BIG,
+                                    margin_top=Size.MEDIUM_BIG.value,
                                     width="100%"
                                 ),
                                 rx.cond(
                                     (TestState.pag_actual + 1) == TestState.total_pages,
                                     rx.center(
                                         alert_dialog("Finalizar test", "¿Has revisado todas tus respuestas? Esta acción no es reversible.", "Revisar", "Sí, finalizar", TestState.finalizar_test),
-                                        margin_top=Size.DEFAULT,
+                                        margin_top=Size.DEFAULT.value,
                                     ),
                                     rx.fragment(),
                                 )
@@ -108,20 +108,19 @@ def index():
                 max_width= "50em",
                 margin = "0 auto",
                 margin_top="100px",
-                margin_bottom="2em",
+                margin_bottom="4em",
                 background= rx.color_mode_cond(light="white", dark=Color.TEXT),
                 padding= "3.1em 1.8em",
-                height="100vh",
-                overflow_y="auto",
+                min_height="50vh",
                 border_radius="10px",
                 align= "center",
                 box_shadow="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
             ),
             align="center",
             width="100%",
-            height="100vh",
+            min_height="100vh",
         ),
-        height="100vh",
+        min_height="100vh",
         width="100%",
         background="linear-gradient(rgba(0,0,0,0.8), rgba(27,154,175,0.8)), url('/tropa.jpg')",
         background_size="cover",
